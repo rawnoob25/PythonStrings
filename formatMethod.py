4a877af3c605db5913097ef45f9ee5e745988a03
Raw Noob <rawNoob25@gmail.com>
2017-07-28 00:42:52 -0400

    update formatMethod.py

--- formatMethod.py
+++ formatMethod.py
@@ -3,11 +3,14 @@ Illustrates use of python's format method
 """
 
 def sameLenOrNot(s,t):
+    """
+    Returns true if and only if strings s and t are the
+    same length
+    Precondition: s and t are both strings
+    """
     if len(s) == len(t):
         print("{0} and {1} are the same length".format(s,t))
     else:
         print("the length of {0} is {1} while the length of {2}\
  is {3}".format(s,len(s),t,len(t)))
-
-
         
